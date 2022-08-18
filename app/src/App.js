0f15cfc59f6275f75c11536b468bff0ca8478d60
qo <72982727+qo@users.noreply.github.com>
2022-08-18 10:38:03 +0300

    feat: add useRef project
    In this project, useRef hook is used to:
    - Print the amount of renders. If useState is used to store renderCount variable, setRenderCount inside a useEffect hook will run an infinite loop. If useRef used instead, this issue will not occur and amount of renders will be shown correctly, because updating useRef variable doesn't cause a rerender.
    - Set user's name to the "Default Value" without rerender.
    - Store previous name (previous state of name state variable). This wouldn't be possible to store with useState, because setting prevName via setPrevName would cause a rerender.

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,60 @@
 import './App.css';
+import {useEffect, useRef, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+  const [name, setName] = useState("");
+
+  const renderCount = useRef(0);
+
+  // const [renderCount, setRenderCount] = useState(0);
+  // Infinite loop!
+  // useEffect(() => {
+  //     // setter will cause the page to rerender
+  //     setRenderCount(prev => prev + 1);
+  // });
+
+  useEffect(() => {
+    // useRef will not cause the page to rerender
+    renderCount.current = renderCount.current + 1;
+  });
+
+  const inputRef = useRef();
+
+  function autoComplete() {
+    // This will update the input value,
+    // but it won't update the state
+    // and won't rerender!
+    inputRef.current.value = "Default Value";
+  }
+
+  // Wouldn't work, since it would store
+  // the same value as the name does
+  // const [prevName, setPrevName] = useState("");
+  //
+  // useEffect(() => {
+  //     setPrevName(name);
+  // }, [name]);
+
+  const prevName = useRef("");
+
+  useEffect(() => {
+    console.log(name);
+    // This update prevName to the current name,
+    // but doesn't rerender, so the name
+    // before that is shown
+    prevName.current = name;
+  }, [name]);
+
+  return (
+      <div className="App">
+        <div className="page">
+          <input ref={inputRef} value={name} onChange={(e) => setName(e.target.value)}/>
+          <h1>My name is {name} and it used to be {prevName.current}</h1>
+          <button onClick={autoComplete}>Auto Complete</button>
+          <h2>Rendered {renderCount.current} times</h2>
+        </div>
+      </div>
   );
 }
 
