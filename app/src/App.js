d639a133f8af75cbfc2dcf4e92172727e69743b5
qo <72982727+qo@users.noreply.github.com>
2022-08-18 14:00:56 +0300

    feat: add useMemo project
    - Add time-consuming double(number) dummy function that returns number multiplied by two, but beforehand runs a for loop with 10^9 iterations.
    - Create a number state variable.
    - Update the number every time user changes it via an input and print the number doubled via double(number) function.
    - Add a button that changes the "theme" via a dark state variable.
    
    In this project, useMemo hook is used to:
    - Memoize previous double value, so the double(number) function is being run again if number has changed since last render. This way, this time-consuming function won't run when the theme is changed.
    - Make effect run only when styles object changes instead of running it when the reference to the object changes.

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,39 @@
 import './App.css';
+import {useEffect, useMemo, useState} from "react";
+
+function double(number) {
+    // Imitating some time-consuming activity
+    for (let i = 0; i < Math.pow(10, 9); i++) {}
+    return 2 * number;
+}
 
 function App() {
+
+  const [number, setNumber] = useState(0);
+  const doubled = useMemo(() => double(number), [number]);
+
+  const [dark, setDark] = useState(false);
+
+  // If not using useMemo here,
+  // useEffect below will run
+  // every time component renders,
+  // because of referential equality
+  const styles = useMemo(() => {
+      return {
+          color: dark ? "white" : "black",
+          backgroundColor: dark ? "black" : "white",
+          border: "1px solid black",
+          padding: "10px",
+      }
+  }, [dark]);
+
+  useEffect(() => console.log("styles changed"), [styles]);
+
   return (
     <div className="App">
-
+      <input value={number} onChange={e => setNumber(e.target.value)} style={styles} />
+      <p style={styles}>{doubled}</p>
+      <button style={styles} onClick={() => setDark(prev => !prev)}>Change Theme</button>
     </div>
   );
 }
